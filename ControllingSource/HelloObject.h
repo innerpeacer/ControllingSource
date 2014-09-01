dca0c5548888f5d55db0af216cd9c97514b9465c
bo chen <bo.chen@palmaplus.com>
2014-09-01 17:48:41 +0800

    Add a Object

---
+++ ControllingSource/HelloObject.h
@@ -0,0 +1,13 @@
+//
+//  HelloObject.h
+//  ControllingSource
+//
+//  Created by innerpeacer on 14-9-1.
+//  Copyright (c) 2014年 Palmap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelloObject : NSObject
+
+@end
---
+++ ControllingSource/HelloObject.m
@@ -0,0 +1,13 @@
+//
+//  HelloObject.m
+//  ControllingSource
+//
+//  Created by innerpeacer on 14-9-1.
+//  Copyright (c) 2014年 Palmap. All rights reserved.
+//
+
+#import "HelloObject.h"
+
+@implementation HelloObject
+
+@end
